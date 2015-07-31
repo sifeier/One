942e1dc0865f92f744be41494c1c66530319959a
buke <buke@taobao.com>
2015-07-31 18:55:56 +0800

    try :)

---
+++ app/src/main/java/com/one/service/MyDogService.java
@@ -0,0 +1,17 @@
+package com.one.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class MyDogService extends Service {
+
+    public MyDogService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
